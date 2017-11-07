47b8f0f353e2cee716d78ea88125096261c9fc06
vinod-boga <vinodboga17@gmail.com>
2017-11-07 14:57:03 +0000

    Done

--- q01_plot_corr/build.py
+++ q01_plot_corr/build.py
@@ -1,8 +1,17 @@
 # Default imports
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
 import pandas as pd
-from matplotlib.pyplot import yticks, xticks, subplots, set_cmap
+import seaborn as sns
 
 data = pd.read_csv('data/house_prices_multivariate.csv')
 
 
 # Write your solution here:
+def plot_corr(data,size=11):
+    plt.figure(figsize=(15,12))
+    cmap = sns.diverging_palette(220, 20, as_cmap=True)
+    sns.heatmap(data.corr(),cmap=cmap)
+    plt.show()
+plot_corr(data)
